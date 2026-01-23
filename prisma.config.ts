32ac569dd893331299b91d837928b474702fabb5
Saajaadeen M. Jeffries <saajaadeen.jeffries@travisspark.com>
2026-01-23 12:22:56 -0800

    docker file testing

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "prisma/config";
 import { config } from "dotenv";
 
-// Load environment variables from .env file
 config();
 
-export default defineConfig({
+export default {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
     seed: 'tsx prisma/seed.ts',
   },
-  datasource: {
-    url: process.env.DATABASE_URL,
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL!,
+    },
   },
-});
+};
